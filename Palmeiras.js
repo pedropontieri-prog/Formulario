bcea097aa7a31d8dd1761b5488f0ad31b0761a1e
pedropontieri-prog <pedro.pontieri@aluno.ifsp.edu.br>
2026-04-24 14:13:32 -0300

    Update Palmeiras.js

--- Palmeiras.js
+++ Palmeiras.js
@@ -1,5 +1,10 @@
-const formulario = document.getElementById('form');
-formulario.addEventListener('submit',function(evento){
+const formulario = document.getElementById('meuFormulario');
+
+formulario.addEventListener('submit', function (evento) {
+  evento.preventDefault(); // evita recarregar a página
+
+  console.log("Formulário enviado!");
+});
 
 function alterarFundo() {
   document.body.style.background = "#222";
